a3b49a6e02b627a8c95693b286bcddaf3eb8c96f
Vinit Keshri <139075087+vinitkesh@users.noreply.github.com>
2023-11-13 06:38:51 +0000

    creating file

---
+++ PreorderToBST.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<stdlib.h>
+// we will take a string of interger and creatre a binary search tree out of it
+
+struct node{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+typedef struct node *noderef;
+
+noderef createNode(int data){
+    noderef newNode = (noderef)malloc(sizeof(struct node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+//CREATING BST FROM PREORDER
+noderef createBSTfrompreorder(noderef root, int data){
+    if(root == NULL){
+        return createNode(data);
+    }
+    if(data < root->data){
+        root->left = createBSTfrompreorder(root->left, data);
+    }
+    else{
+        root->right = createBSTfrompreorder(root->right, data);
+    }
+    return root;
+}
+//INORDER PRINTING
+void inorderprint(noderef root){
+    if(root == NULL){
+        return;
+    }
+    inorderprint(root->left);
+    printf("%d ", root->data);
+    inorderprint(root->right);
+}
+//POSTORDER PRINTING
+void postorder(noderef root){
+    if(root == NULL){
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+//PREORDER PRINTING
+void preorder(noderef root){
+    if(root == NULL){
+        return;
+    }
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+
+
+int main(){
+    int arr[7]={100,20,10,30,200,150,300};
+
+    noderef root=createNode(arr[0]);
+    noderef temp = root;
+    
+    for(int i=1; i<7; i++){
+        root = createBSTfrompreorder(root, arr[i]);
+    }
+    inorderprint(temp);
+    printf(" - inorder\n");
+    preorder(temp);
+    printf("- preorder\n");
+    postorder(temp);
+    printf("- postorder\n");
+    return 1;
+}
