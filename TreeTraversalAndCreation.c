b6f194111d4c0c318585ed5e3dce8458764c24ea
Vinit Keshri <139075087+vinitkesh@users.noreply.github.com>
2023-11-13 06:27:55 +0000

    create TreeTraversalAndCreation.c

--- TreeTraversalAndCreation.c
+++ TreeTraversalAndCreation.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct node
 {
@@ -7,7 +8,7 @@ struct node
     struct node *right;
 };
 
-typedef struct node noderef;
+typedef struct node *noderef;
 
 noderef createNode(int data)
 {
@@ -18,16 +19,31 @@ noderef createNode(int data)
     return newNode;
 }
 
-noderef createTreePostorder(int postorder[], int n)
+noderef createTreePostorder(int postorder[], int start, int end)
 {
-    noderef root = createNode(postorder[n - 1]);
-    noderef temp = root;
+    if (start > end)//if start is greater than end then there is no node left to be inserted
+        return NULL;// so the leaf node is pointing to null
 
-    for (int i = n - 2; i >= 0; i--)
-    {
-        
+    noderef root = createNode(postorder[end]);//last elemet of postorder is the root
+    
+    if (start == end)//if there is only one element left then return the root
+        return root;
+
+    int i;
+    for (i = end - 1; i >= start; i--){
+        if (postorder[i] < root->data)
+            break;
     }
+
+    root->right = createTreePostorder(postorder, i + 1, end - 1);
+    root->left = createTreePostorder(postorder, start, i);
+
     return root;
+}
+
+noderef createTreeInorder (int inorder[], int start, int end)
+{
+    printf("you can't create a tree with only inorder traversal\n");
 }
 
 //Functions
@@ -42,11 +58,11 @@ int main()
     scanf("%d", &choice);
     printf("Enter the number of nodes in the tree\n");
             int n;
-            scanf("%d", &n);
+            scanf("%d", &n);int postorder[n]; int preorder[n];int inorder[n];
     switch (choice){
 
         case 1:
-            int postorder[n];
+            
             printf("Enter the postorder traversal of the tree\n");
             for (int i = 0; i < n; i++)
             {
@@ -54,7 +70,7 @@ int main()
             }
             break;
         case 2:
-            int preorder[n];
+           
             printf("Enter the preorder traversal of the tree\n");
             for (int i = 0; i < n; i++)
             {
@@ -62,12 +78,33 @@ int main()
             }
             break;
         case 3:
-            int inorder[n];
+            
             printf("Enter the inorder traversal of the tree\n");
             for (int i = 0; i < n; i++)
             {
                 scanf("%d", &inorder[i]);
             }
+            break;
+        default:
+            printf("Invalid choice\n");
+            break;
+    }
+
+    printf("The tree is created\n");
+    printf("1. print tree in inorder\n");
+    printf("2. print tree in preorder\n");
+    printf("3. print tree in postorder\n");
+    scanf("%d", &choice);
+    switch (choice){
+
+        case 1:
+            printf("The inorder traversal of the tree is\n");
+            break;
+        case 2:
+            printf("The preorder traversal of the tree is\n");
+            break;
+        case 3:
+            printf("The postorder traversal of the tree is\n");
             break;
         default:
             printf("Invalid choice\n");
